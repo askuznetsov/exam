fd8b849dde1f789a3ae8916b951339c49a93cc5d
Oleksandr KUZNETSOV <okuznets@e1r9p10.unit.ua>
2018-03-02 17:44:59 +0200

    search_and_replace

---
+++ lvl1/search_and_replace.c
@@ -0,0 +1,72 @@
+/*  Expected files   : search_and_replace.c
+    Allowed functions: write, exit
+--------------------------------------------------------------------------------
+
+Write a program called search_and_replace that takes 3 arguments, the first
+arguments is a string in which to replace a letter (2nd argument) by
+another one (3rd argument).
+
+If the number of arguments is not 3, just display a newline.
+
+If the second argument is not contained in the first one (the string)
+then the program simply rewrites the string followed by a newline.
+
+Examples:
+$>./search_and_replace "Papache est un sabre" "a" "o"
+Popoche est un sobre
+$>./search_and_replace "zaz" "art" "zul" | cat -e
+$
+$>./search_and_replace "zaz" "r" "u" | cat -e
+zaz$
+$>./search_and_replace "jacob" "a" "b" "c" "e" | cat -e
+$
+$>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
+ZaZ eT David aiME le METal.$
+$>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
+eNcOre Un ExEmPle Pas Facile a Ecrire $
+--------------------------------------------------------------------------*/
+
+#include <unistd.h>
+
+void    ft_putchar(char temp) {
+    write(1, &temp, 1);
+}
+
+int     ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int     main(int argc, char **argv)
+{
+    int i;
+
+    i = 0;
+    if (argc != 4)
+    {
+        ft_putchar('\n');
+        return (0);
+    }
+    if ((ft_strlen(argv[2]) > 1) || (ft_strlen(argv[3]) > 1))
+    {
+        ft_putchar('\n');
+        return (0);
+    }
+    while (argv[1][i])
+    {
+        if (argv[1][i] == argv[2][0])
+        {
+            ft_putchar(argv[3][0]);
+        }
+        else
+            ft_putchar(argv[1][i]);
+        i++;
+    }
+    ft_putchar('\n');
+    return (0);
+}
